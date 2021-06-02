199da4298ce72d57e5bb10b30447841dc2b80ba9
xs1hao <42058817+xs1hao@users.noreply.github.com>
2021-06-03 00:00:44 +0800

    2021年6月3日00:00:34

--- src/components/bar.vue
+++ src/components/bar.vue
@@ -1,25 +1,29 @@
 <template>
   <div class="container">
-
-     In bar component~ 
-
+    In bar component~
+    <Counter :names="'王二小'"
+             @parent-log="parentLog($event)"></Counter>
   </div>
 </template>
 
 <script>
+import Counter from './counter/counter.vue'
 export default {
   name: '',
   data() {
     return {
-
+      
     }
   },
+  methods: {
+    parentLog(eve) {
+      console.log(eve)
+    },
+  },
   components: {
-
-  }
+    Counter,
+  },
 }
 </script>
 
-<style lang="less" scoped>
-
-</style>
+<style lang="less" scoped></style>
---
+++ src/components/counter/counter.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="counter">
+     <p>
+         {{this.count}}
+     </p>
+    <button @click="add()">加一个</button>
+    <button @click="sub()">减一个</button>
+    <button @click="pLog()">Parent加一个</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'counter',
+  props: {
+      count: {
+          type: Number,
+          default: 13,
+          required: false,
+          validator: function(value) {
+              return value > 0;
+          }
+      },
+      names: {
+          type: String,
+          required: true,
+          validator: function(value) {
+              return value.length > 1;
+          }
+      }
+  },
+  data() {
+    return {
+        
+    }
+  },
+  methods: {
+      add(){
+          this.count++;
+      },
+      sub(){
+          this.count--;
+      },
+      pLog() {
+          this.$emit('parent-log', this.names)
+      }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+    button{
+        margin: 10px;
+    }
+</style>
